20a2628a92fcf7ff6c8fd9c680b59a66053fbbbe
Koha <BelkoZdorovo@yandex.ru>
2022-12-14 20:25:32 +0300

    alpha build

--- Fractional_calc.py
+++ Fractional_calc.py
@@ -1,1 +1,17 @@
+from fractions import Fraction as F
+
+
+def fraction(user_number_1, user_number_2, operation):
+    user_number_1 = F(user_number_1).limit_denominator(10)
+    user_number_2 = F(user_number_2).limit_denominator(10)
+    if operation in ('+', '-', '*', '/'):
+        if operation == '+':
+            return str(user_number_1 + user_number_2)
+        elif operation == '-':
+            return str(user_number_1 - user_number_2)
+        elif operation == '*':
+            return str(user_number_1 * user_number_2)
+        elif operation == '/':
+            return str(user_number_1 / user_number_2)
+
 
