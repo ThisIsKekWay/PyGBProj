22f9f5f0b1fa045df99a9bdd707182c53b468228
ThisIsKekWay <113125791+ThisIsKekWay@users.noreply.github.com>
2022-12-15 20:43:17 +0300

    Revert "Update_Frac"

--- Complex_calc.py
+++
@@ -1,8 +0,0 @@
-def arithmetic(a, b, symbol_operator): # a и b должны быть тип list  a = [], b =[]
-    complex1 = complex(a[0], a[1])
-    complex2 = complex(b[0], b[1])
-    operation = {'+': lambda x, y: x + y,
-                 '-': lambda x, y: x - y,
-                 '*': lambda x, y: x * y,
-                 '/': lambda x, y: x / y}
-    return f'{complex1} {symbol_operator} {complex2} = {operation[symbol_operator](complex1,complex2)}'
